12431880fbf44359ed3d20011ccf259f495d8c48
Quadratech188 <quadratech188@gmail.com>
2025-09-27 22:02:46 +0900

    fix: Read/Write len before message

--- native/src/main.rs
+++ native/src/main.rs
@@ -1,5 +1,5 @@
 use serde;
-use std::{fs, io::ErrorKind, path};
+use std::{fs::{self, read}, io::{self, stdout, ErrorKind, Read, Write}, path};
 
 #[derive(serde::Deserialize)]
 #[serde(rename_all = "snake_case")]
@@ -53,17 +53,44 @@ enum Return {
     Error {error_type: ErrorType, message: String}
 }
 
+fn read_message() -> std::io::Result<String> {
+    let mut len_buf = [0u8; 4];
+    io::stdin().read_exact(&mut len_buf)?;
+    let len = u32::from_ne_bytes(len_buf) as usize;
+    let mut buf = vec![0u8; len];
+    io::stdin().read_exact(&mut buf)?;
+
+    let s = String::from_utf8(buf).map_err(|err| {
+        io::Error::new(ErrorKind::InvalidInput, err)
+    })?;
+
+    Ok(s)
+}
+
+fn write_message(msg: &String) -> io::Result<()> {
+    let len = msg.len() as u32;
+    stdout().write_all(&len.to_ne_bytes())?;
+    stdout().write_all(&msg.as_bytes())?;
+    stdout().flush()?;
+
+    Ok(())
+}
+
 fn report_error(error_type: ErrorType, err: &String) {
     let result = Return::Error {
         error_type: error_type,
         message: err.to_string()
     };
-
-    serde_json::to_writer(std::io::stdout(), &result).unwrap();
+    let _ = write_message(&serde_json::to_string(&result).unwrap());
 }
 
 fn main() {
-    let request: Request = match serde_json::from_reader(std::io::stdin()) {
+    let message = read_message();
+    if let Err(err) = &message {
+        report_error(ErrorType::Other, &err.to_string());
+    }
+    let message = message.unwrap();
+    let request: Request = match serde_json::from_str(&message) {
         Ok(req) => req,
         Err(e) => {
             report_error(ErrorType::Other, &e.to_string());
@@ -79,6 +106,7 @@ fn main() {
         if request.options.create_dest_folder {
             // TODO
             report_error(ErrorType::Other, &"create_dest_folder is not yet supported".into());
+            return;
         }
         else {
             report_error(ErrorType::DestDirNoExists, &format!("Destination directory {} does not exist", request.dest.parent().unwrap().display()));
@@ -90,6 +118,7 @@ fn main() {
         match request.options.already_exists {
             AlreadyExists::Replace => {
                 report_error(ErrorType::Other, &"replace is not yet supported".into());
+                return;
             },
             AlreadyExists::Throw => {
                 report_error(ErrorType::DestExists, &format!("Destination {} already exists", request.dest.display()));
@@ -133,5 +162,5 @@ fn main() {
             return;
         }
     }
-    serde_json::to_writer(std::io::stdout(), &Return::Success).unwrap();
+    let _ = write_message(&serde_json::to_string(&Return::Success).unwrap());
 }
