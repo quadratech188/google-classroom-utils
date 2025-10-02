811f216127f9f2fa320e8ac8674928cd6d33f7c5
Quadratech188 <quadratech188@gmail.com>
2025-10-03 00:54:26 +0900

    fix: Capture classroom id properly

--- extension/util.js
+++ extension/util.js
@@ -13,10 +13,10 @@ async function listen_once(event, cond) {
 }
 
 function get_classroom_id(url) {
-	const re = /https:\/\/classroom\.google\.com(\/u\/\d+)?\/c\/([^\/]+)/
+	const re = /https:\/\/classroom\.google\.com(?:\/u\/\d+)?\/c\/(?<id>[^\/]+)/
 	const match = url.match(re);
 	if (match === null) {
 		throw `Failed to parse current url: ${url}`;
 	}
-	return match[1];
+	return match.groups.id;
 }
